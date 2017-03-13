0a5a1e320129f9f05af91b93a2323428d811811c
Eddie Wood <ewood111@yahoo.com>
2017-03-13 04:14:54 -0700

    add features persist bill amount, tip_precentage, 10 minutes timeout, settings animation  work in progress

--- tippy_December2016/SettingsViewController.swift
+++ tippy_December2016/SettingsViewController.swift
@@ -13,13 +13,13 @@ class SettingsViewController:
 {
     
     @IBOutlet weak var picker1: UIPickerView!
-    
      var picker_Data: [String] = [String]()
      var picker_percent: [String] = [String]()
-    var tip_selected: String = " "
-    
+     var tip_selected: String = " "
     @IBOutlet weak var picker_Sel_Label: UILabel!
-    
+    let UD_tipPCent: String = "UD_tip_PC_save"
+//  let UD_billAmt:  String = "UD_bill_Amount"
+//  let UD_saveDate: String = "UD_save_Date"
     
     override func viewDidLoad()
     {
@@ -41,18 +41,34 @@ class SettingsViewController:
               "22.5",
               "25"   ]
         
-       let userDefaults = Foundation.UserDefaults.standard
-        
-        picker_Sel_Label.text = userDefaults.string(forKey: "tipDefault")
-        
+        let userDefaults = Foundation.UserDefaults.standard
+        picker_Sel_Label.text = ""
+        if (userDefaults.value(forKey: UD_tipPCent) != nil)
+        {
+//            let formatter2 = DateFormatter.init()
+//            print("SVC_Debug M_020 [",
+//               formatter2.string(from: 
+//            userDefaults.value(forKey: UD_tipPCent) 
+//                as! Date), "]")
+            let savedTPC = userDefaults.value(forKey: UD_tipPCent)
+            print("SVC_Debug_015 [", savedTPC as Any, "]")
+            if (savedTPC is Date) { } else
+            {
+              picker_Sel_Label.text = savedTPC as! String?
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.view.backgroundColor = UIColor.cyan
     }
     
     @IBAction func default_Save_Label(_ sender: Any)
     {
         print("Default_Saved_Label:", tip_selected)
         let userDefaults = Foundation.UserDefaults.standard
-        
-        userDefaults.set(tip_selected, forKey: "tipDefault")
+        userDefaults.set(tip_selected, forKey: UD_tipPCent)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -71,13 +87,15 @@ class SettingsViewController:
         picker_Sel_Label.text = picker_percent[row]
         tip_selected =
             picker_Sel_Label.text!
+        let userDefaults = Foundation.UserDefaults.standard
+        userDefaults.set(picker_percent[row],
+            forKey: UD_tipPCent )
     }
   
     override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController)
     {
         print("SettingsVC unwind called:saved tip=", tip_selected)
     }
-    
       
     override func canPerformUnwindSegueAction(_ action: Selector, from fromViewController: UIViewController, withSender sender: Any) -> Bool {
         print("SettingsVC canPerformUnwindSequeAction called:saved tip=", tip_selected)
@@ -86,17 +104,15 @@ class SettingsViewController:
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// Dispose of any resources that can be recreated.
     }
     
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
     */
-    
 }
--- tippy_December2016/ViewController.swift
+++ tippy_December2016/ViewController.swift
@@ -2,69 +2,137 @@
 //  ViewController.swift
 //  tippy_December2016
 //
-//  Created by jpwood on 3/3/17.
-//  Copyright © 2017 jpwood. All rights reserved.
+//  Created by ewood on 3/3/17.
+//  Copyright © 2017 ewood. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     var Saved_Default_tip: String?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    let UD_billAmt:  String = "UD_bill_Amount"
+    let UD_saveDate: String = "UD_save_Date"
+    let UD_tipPCent: String = "UD_tip_PC_save"
+    let locale_cur = Locale.current
+//    let locale_symbol =  locale_cur.symbol
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var SavedTipDef_Label: UILabel!
+    @IBAction func billResetButton(_ sender: Any)
+    {
         let userDefaults = Foundation.UserDefaults.standard
-        SavedTipDef_Label.text =      userDefaults.string(forKey: "tipDefault")
-        print ("ViewController_DidLoad")
-        print (SavedTipDef_Label.text)
-      
+        userDefaults.set("", forKey:UD_billAmt)
+        billField.text = ""
+        view.endEditing(false)
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        print ("VC_VDL01:currency_symbols=[", locale_cur, "]")
+//        switch (locale_cur) {
+//            case "en_UK": print("VC_VDL12: en_uk")
+//            case "en_US": print("VC_VDL14: en_us"
+//            default     : print("VC_VDL29: default")
+//        }
+//          self.onTap(billField)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        let now = Foundation.Date()
+        printLog(log: "M_viewWillAppear_10" as AnyObject)
+        let userDefaults = Foundation.UserDefaults.standard
+        var saveDate: Date! = nil
+        
+        let formatter: DateFormatter = DateFormatter.init()
+        if (userDefaults.value(forKey:UD_saveDate) != nil)
+        {
+            saveDate = userDefaults.value(forKey: UD_saveDate) as! Date
+            let expireTime: Date =
+                saveDate.addingTimeInterval(600)
+            let saveDateC = formatter.string(from: saveDate)
+            let expireTimeC = formatter.string(from: expireTime)
+            print("ViewWillAppear_20: Saved Date()=",  (saveDate), " now= ",  (now),
+                  " ExpireTime= ",  (expireTime))
+            
+            let notExpire: Bool = expireTime > now
+            if (notExpire) {
+                  print("M_VWAppear_22 not expired")
+             } else  {
+                  print("M_VWAppear_24 yes expired")
+                  userDefaults.set("", forKey:UD_billAmt)
+                  billField.text = ""
+            }
+        }
+        
+        var billAmt: String = "";
+        if (userDefaults.value(forKey: UD_billAmt) != nil)
+        {
+            billAmt = userDefaults.value(forKey:UD_billAmt) as! String
+            print("ViewWillAppear_30 saved billAmt:", (billAmt) )
+            billField.text = billAmt
+        }
+        
+        let tip_from_Settings: String = ""
+        SavedTipDef_Label.text = ""
+        let formatter1 = DateFormatter.init()
+        if (userDefaults.value(forKey: UD_tipPCent) != nil)
+        {
+           let tip_settings = userDefaults.value(forKey: UD_tipPCent)
+            
+//           print("M_WWA_Debug_035[",formatter1.string(from: tip_settings as! Date),"]")
+           SavedTipDef_Label.text = tip_settings as! String?
+//         SavedTipDef_Label.text = tip_from_Settings
+        }
+        
         switch (SavedTipDef_Label.text)
         {
         case "10"?:
-            tipControl.setEnabled(true, forSegmentAt: 0)
+            tipControl.selectedSegmentIndex = 0
         case "12.5"?:
-            tipControl.setEnabled(true, forSegmentAt: 1)
+            tipControl.selectedSegmentIndex = 1
         case "15"?:
-            tipControl.setEnabled(true, forSegmentAt: 2)
+            tipControl.selectedSegmentIndex = 2
         case "17.5"?:
-            tipControl.setEnabled(true, forSegmentAt: 3)
+            tipControl.selectedSegmentIndex = 3
         case "20"?:
-            tipControl.setEnabled(true, forSegmentAt: 4)
+            tipControl.selectedSegmentIndex = 4
         case "22.5"?:
-            tipControl.setEnabled(true, forSegmentAt: 5)
+            tipControl.selectedSegmentIndex = 5
         case "25"?:
-            tipControl.setEnabled(true, forSegmentAt: 6)
-        default: print ("Error in ViewController Segmented Control locEyeCatch:1010")
+            tipControl.selectedSegmentIndex = 6
+        default: print (
+            "Error in View_C Seg_Control_55[", SavedTipDef_Label.text as Any, "]")
         }
-        printLog(log: "From main viewDidLoad" as AnyObject?)
-    }
-
-    override func viewWillAppear(_ animated: Bool)
-    {
-        printLog(log: "From main viewWillAppear" as AnyObject)
         
-          let userDefaults = Foundation.UserDefaults
-          var tip_from_Settings = userDefaults.standard.value(forKey: "tipDefault")
-                
-          printLog(log: tip_from_Settings as AnyObject)
-          SavedTipDef_Label.text = tip_from_Settings as! String?
+        var secsElapse: Int = 0
+        if (saveDate != nil) {
+            secsElapse = Int(now.timeIntervalSince(saveDate))
+            print("SettingsCntlr VVA_60:", saveDate, now, secsElapse)
+        }
+        
+        //        imageView1.animatew
+        imageView1.startAnimating()
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var tipControl: UISegmentedControl!
-
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    
-    @IBOutlet weak var SavedTipDef_Label: UILabel!
     
     @IBAction func seg_value_Changed(_ sender: Any) {
+        
+//        var local_currency: String = currency_symbols
+        
         let tipPercentages =
             [ 0.1, 0.125, 0.15, 0.175,0.2, 0.225, 0.25 ]
         
@@ -72,11 +140,26 @@ class ViewController: UIViewController {
         let  tip = bill * (tipPercentages[tipControl.selectedSegmentIndex]);
         let total = bill + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
+//        tipLabel.text = currency_symbol ?? ""
+        tipLabel.text =  String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
-    @IBAction func calculateTip(_ sender: Any) {
+    @IBAction func save_tip_Amount(_ sender: Any)
+    {
+        let userDefaults = Foundation.UserDefaults.standard
+        userDefaults.set(billField.text, forKey:UD_billAmt)
+        let now: Date = Foundation.Date()
+        userDefaults.set(now, forKey:UD_saveDate)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any)
+    {
+        let now: Date = Foundation.Date()
+        let userDefaults = Foundation.UserDefaults.standard
+        userDefaults.set(billField.text, forKey:"billAmount")
+        userDefaults.set(now, forKey:UD_saveDate)
+        
         let tipPercentages =
              [ 0.1, 0.125, 0.15, 0.175,0.2, 0.225, 0.25 ]
 
@@ -86,11 +169,13 @@ class ViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        
     }
-    @IBAction func onTap(_ sender: Any) {
-         view.endEditing(true)
+  
+    
+    @IBAction func onTap(_ sender: Any)    {
+        view.endEditing(true)
     }
+
     
     func printLog(log: AnyObject?) {
         let formatter = DateFormatter()
@@ -100,6 +185,20 @@ class ViewController: UIViewController {
         { print("nil") } else
         { print(log!)   }
     }
+    
+//    func currencySymbol_wholeName(forLocale locale:
+//        Locale = Locale.currentLocale()) -> String
+//    {
+//          var identifier : String = locale.localeIdentifier
+//          switch(identifier) {
+//          case "en_UK":
+//              return "Pounds"
+//          case "en_US":
+//              return "Dollar"
+//          default:
+//              return ""
+//          }
+//    }
     
 }
 
